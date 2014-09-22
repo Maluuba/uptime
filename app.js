54defb54b3112fdef148725995ccf00c01fb07fb
James Moore <james.moore@maluuba.com>
2014-09-22 17:42:18 -0400

    Added logging to webviewAsserts. Added verify = false to app.js

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var PollerCollection = require('./lib/pollers/pollerCollection');
 var apiApp     = require('./app/api/app');
 var dashboardApp = require('./app/dashboard/app');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 // database
 
 var mongoose   = require('./bootstrap');
