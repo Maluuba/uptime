5c66a136d8659595c77157f70b4708ad9bee8e8d
James Moore <james.moore@maluuba.com>
2014-11-07 10:47:38 -0500

    Added run-uptime.sh script to uptime.

---
+++ run-uptime.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while true; do
+        returnVal=$(curl -s --connect-timeout 2 -I http://maluuba:RCjvO3vhSP2C8vIfEQ^ats@localhost:8082/dashboard/events | grep HTTP | cut -d ' ' -f 2)
+        if [ -z "$returnVal" ] || [ ${returnVal} -ne 200 ]; then
+            pidVal=$(ps aux | grep "nodejs"  | grep -v "grep" |  tr -s ' ' | cut -d ' ' -f 2)
+            kill -9 ${pidVal}
+            nodejs app  &
+        else
+            echo "Uptime is fine"
+        fi
+        sleep 300s
+done
